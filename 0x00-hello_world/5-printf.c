ffe4ccfca1d9b1335e96449a8292e914a4930797
wimade <wtlab2020@gmail.com>
2022-06-02 10:29:06 -0700

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - It prints a function
+ *
+ * Description: It print a function
+ *
+ * Return: 0
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
