0a68e62b16b102032739f05e2093d6bc027ea617
wimade <wtlab2020@gmail.com>
2022-06-02 05:01:24 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <studio.h>
+
+/**
+ * main: print the string in the puts function
+ *
+ * Description:using the main function
+ * this prints "Programming is like building a multilingual puzzle"
+ * return: 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
