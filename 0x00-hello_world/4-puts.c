77cab124f0b990dd4e22b89219e6ab8097e8adf1
wimade <wtlab2020@gmail.com>
2022-06-02 08:51:04 -0700

    wireless

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,7 @@
  * main - print the string in the puts function
  * Description: prints "Programming is like building a multilingual puzzle
  * Return: 0
- */
+*/
 int main(void)
 {
 puts("\"Programming is like a multilingual puzzle");
