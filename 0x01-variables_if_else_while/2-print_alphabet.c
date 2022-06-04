c38e575ef35a71d73b5a1f0bf33becd5ec6c697d
wimade <wtlab2020@gmail.com>
2022-06-03 17:44:29 -0700

    Print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - print out the result from the function
+ *
+ * Description: use to print alphabet from a to z
+ *
+ * Return: return interger value o
+ *
+ */
+int main(void)
+{
+	int alphabet;
+
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+	{
+		putchar(alphabet);
+	}
+	putchar('\n');
+	return (0);
+}
