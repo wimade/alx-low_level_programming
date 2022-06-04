f2a800ec8f5c6a435b47d92b878a4bc65baaecf7
wimade <wtlab2020@gmail.com>
2022-06-04 15:36:09 -0700

    Print comb3

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - return the result of the function
+ *
+ * Description: print the required results
+ *
+ * Return: return the integer value 0
+ *
+ */
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 9; i++)
+	{
+		for (j + i + 1; j < 10; j++)
+		{
+			putchar(i + '0');
+			putchar(j + '0');
+			if (i == 8 && j == 9)
+				break;
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
