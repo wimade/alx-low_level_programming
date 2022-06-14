3950e1e4aa3f6e654d2da6909132f6d9ab0d6dc3
wimade <wtlab2020@gmail.com>
2022-06-13 17:44:37 -0700

    mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * mul - multiply two integers
+ * @a: integer one to be multiplied
+ * @b: integer two to be multiplied
+ *
+ * Description: print out required result
+ *
+ * Return: return ingteger value
+ *
+ */
+int mul(int a, int b)
+{
+	int result;
+
+	result = a * b;
+	return (result);
+}
