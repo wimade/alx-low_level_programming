f122ad0759088c89bab31cbbe8be835e7366e0ac
wimade <wtlab2020@gmail.com>
2022-06-14 04:20:09 -0700

    more numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ *
+ * more_numbers - print 0 through 14 ten times
+ *
+ * Description: print out the required result
+ *
+ * Return: return void
+ *
+ */
+void more_numbers(void)
+{
+	int i;
+	int x;
+
+	for (x = 0; x < 10; x++)
+	{
+		for (i = 0; i <= 14; i++)
+		{
+			if (i > 9)
+				_putchar((i / 10) + '0');
+			_putchar((i % 10) + '0');
+		}
+		_putchar('\n');
+}
