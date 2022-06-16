3c1acec3f139f71cadd2569576dff0c125dcdc05
wimade <wtlab2020@gmail.com>
2022-06-16 14:38:14 -0700

    Abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _abs - get the absolute number of an integer
+ * @x: the value that function retur n the absolute number
+ *
+ * Description: return the required results
+ *
+ * Return: return integer value 0
+ *
+ */
+int _abs(int x)
+{
+	if (x >= 0)
+		return (x);
+	else
+		return (x * -1);
+	return (0);
+
+}
---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_last_digit -prints last digit of integer input
+ * @n: integer to modify
+ *
+ * Return: 1
+ */
+
+int print_last_digit(int n)
+{
+	int l, p;
+
+	l = n % 10;
+	if (1 < 0)
+		1 = -1;
+	p = '0' + 1;
+	_putchar(p);
+	return (1);
+}
