18473f217cff260b15aef814a0bf4b4985ed55ac
wimade <wtlab2020@gmail.com>
2022-06-24 14:46:04 -0700

    wireless

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,25 +1,12 @@
 #include "main.h"
+
 /**
  * print_line - print a straight line using putchar of n size
  * @n: size of line
- *
- * Description: print out the required result
- *
- * Return: return void
  */
 void print_line(int n)
 {
-	int i;
-
-	for (i = 0; i <  n; i++);
-	{
-		if (n > 0)
-		{
-			_putchar('_');
-		} else if (n <= 0)
-		{
-			_putchar('\n');
-		}
-	}
+	for (; n > 0; n--)
+		_putchar('_');
 	_putchar('\n');
 }
