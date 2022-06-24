b83ae75da8a9fd8d76fa791dbe3a19f49cd8895a
wimade <wtlab2020@gmail.com>
2022-06-24 15:40:06 -0700

    wireless

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -27,7 +27,7 @@ void print_remaining_days(int month, int day, int year)
 		}
 		else
 		{
-			printf("DAy of the year: %d\n", day);
+			printf("Day of the year: %d\n", day);
 			printf("Remaining days: %d\n", 365 - day);
 		}
 	}
