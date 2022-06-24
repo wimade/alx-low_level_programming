a63e52e96d1b0a0afd1591485614feafbc7d6683
wimade <wtlab2020@gmail.com>
2022-06-24 15:05:58 -0700

    wireless

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,19 +1,14 @@
 #include "main.h"
+
 /**
  * print_numbers - print all numbers 0 through 9 and then new line
- *
- * Description: print out the required result
- *
- * Return: return void
- *
  */
+
 void print_numbers(void)
 {
-	 int i;
+	int i;
 
-	 for (i = '0'; i <= '9'; i++)
-	 {
-		 _putchar(i);
-	 }
-	 _putchar('\n');
+	for (i = '0'; i <= '9'; i++)
+		_putchar(i);
+	_putchar('\n');
 }
