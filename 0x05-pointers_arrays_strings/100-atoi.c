4d3fb7f1a9e00f7b3e4224f0ff4dd603c65e6adb
wimade <wtlab2020@gmail.com>
2022-07-03 16:22:43 -0700

    wireless

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -15,7 +15,7 @@ int _atoi(char *s)
 	n = 0;
 	len = 0;
 	f = 0;
-	difit = 0;
+	digit = 0;
 
 	while (s[len] != '\0')
 		len++;
@@ -32,7 +32,7 @@ int _atoi(char *s)
 				digit = -digit;
 			n = n * 10 + digit;
 			f = 1;
-			if (s[i + 1} < '0' || s[i + 1] > '9')
+			if (s[i + 1] < '0' || s[i + 1] > '9')
 				break;
 			f = 0;
 		}
