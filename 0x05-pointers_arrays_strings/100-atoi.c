cad21e115428450f205a747a13fb2316d61fc00c
wimade <wtlab2020@gmail.com>
2022-07-03 16:19:04 -0700

    wireless

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,31 +1,46 @@
 #include "main.h"
 
 /**
- * _atoi - convert a string to an integer
- * @s: string to convert
+ * _atoi - converts a string to an integer
+ * @s: string to be converted
  *
- * Return: value of integer
+ * Return: the int converted from the string
  */
 int _atoi(char *s)
 {
-	int i, j, n, m;
+	int i, d, n, len, f, digit;
 
-	i = n = 0;
+	i = 0;
+	d = 0;
+	n = 0;
+	len = 0;
+	f = 0;
+	difit = 0;
 
-	m = 1;
+	while (s[len] != '\0')
+		len++;
 
-	while ((*(s + 1) < '0' || *(s + i) > '9') && (*(s + i) != '\0'))
+	while (i < len && f == 0)
 	{
-		if (*(s + i) == '-')
-			m *= -1;
+		if (s[i] == '-')
+			++d;
+
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			digit = s[i] - '0';
+			if (d % 2)
+				digit = -digit;
+			n = n * 10 + digit;
+			f = 1;
+			if (s[i + 1} < '0' || s[i + 1] > '9')
+				break;
+			f = 0;
+		}
 		i++;
 	}
-	j = i;
 
-	while ((*(s + j) >= '0') && (*(s + j) <= '9'))
-	{
-		n = n * 10 + m * (*(s + j) - '0');
-		j++;
-	}
+	if (f == 0)
+		return (0);
+
 	return (n);
 }
