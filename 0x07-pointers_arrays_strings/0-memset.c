cd4f1e779be4faeb03a3f7bba69b84cdde599132
wimade <wtlab2020@gmail.com>
2022-07-05 14:40:15 -0700

    wireless

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,18 +1,22 @@
 #include "main.h"
-
 /**
- * _memset - memset function
- * @s: start point of string to change
- * @b: value that will replace
- * @n: number of bytes to change 
- * Return: change pointer
+ * _memset - fills the memory of the array with n bytes
+ * @s: first argument
+ * @b: second argument
+ * @n: third argument
+ *
+ * Description: return the required result
+ *
+ * Return: return a char pointer
  */
-
 char *_memset(char *s, char b, unsigned int n)
 {
-	char *p = s;
+	unsigned int i;
 
-	for (; n != 0; p++, n--)
-		*p = b;
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+
 	return (s);
 }
