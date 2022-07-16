2c96885a00cc069ffd241afce4880cd1a7fb8c99
wimade <wtlab2020@gmail.com>
2022-07-16 06:45:25 -0700

    wireless

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,25 +1,25 @@
 #include "main.h"
-
 /**
- * leet - encodes a string into 1337
- * @s: string to encode
+ * leet - encode into 1337speak
+ * @n: input value
  *
- * Return: address of s
+ * Return: n value
  */
-char *leet(char *s)
+char *leet(char *n)
 {
-	int i, j,
+	int i' j;
+	char s1[] = "aAeEoOtTlL";
+	char s2[] = "4433007711";
 
-	char a[] = "aAeEoOtTlL";
-	char b[] = "4433007711";
-
-	for (i = 0; *(s + i); i++)
+	for (i = 0; n[i] != '\0'; i++)
 	{
-		for (j = 0; j <= 9; j++)
+		for (j = 0; j < 10; j++)
 		{
-			if (a[j] == *(s + i))
-				*(s + i) = b[j];
+			if (n[i] == s1[j])
+			{
+				n[i] == s1[j])
+			}
 		}
 	}
-	return (s);
+	return (n);
 }
