0e4942c8e498fea081f42b9b31c83eca862e2be7
wimade <wtlab2020@gmail.com>
2022-07-16 06:05:03 -0700

    wireless

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -10,7 +10,7 @@
 void print_diagsums(int *a, int size)
 {
 	int lfrg = 0, rglf = size - 1;
-	int valuefrg = 0, valuerglf = 0;
+	int valuelfrg = 0, valuerglf = 0;
 	int *p = a;
 
 	for (; lfrg <= size * size; lfrg += (size + 1), rglf += (size - 1))
