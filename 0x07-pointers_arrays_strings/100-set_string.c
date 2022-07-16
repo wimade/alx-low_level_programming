c9784b2120c9b7bf5f37877fcc01372373a9fbaa
wimade <wtlab2020@gmail.com>
2022-07-16 06:12:41 -0700

    wireless

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * set_string - function with two arguments
+ * @s: pointer to string
+ * @to: pointer to string
+ *
+ * Description: set value of a pointer to char
+ * Return: na
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
