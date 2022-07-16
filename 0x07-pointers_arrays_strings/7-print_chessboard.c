dc8eaa7a5e02974940d0803f1c1247c3941c5aa5
wimade <wtlab2020@gmail.com>
2022-07-16 05:38:57 -0700

    wireless

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -8,13 +8,14 @@
  */
 void print_chessboard(char (*a)[8])
 {
-	int subC, primC = 0;
-	while (primC < 8)
-	{
-		subC = 0;
-		while (subC < 8)
-			_putchar(a[primC][subC++]);
-		_putchar('\n');
-		primC++;
-	}
+int subC, primC = 0;
+while (primC < 8)
+{
+subC = 0;
+while (subC < 8)
+_putchar(a[primC][subC++]);
+_putchar('\n');
+primC++;
 }
+}
+
