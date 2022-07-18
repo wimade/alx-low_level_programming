53037a909b5d404a7445df78cadc17c83dd8bd15
wimade <wtlab2020@gmail.com>
2022-07-18 15:08:07 -0700

    wireless

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 /**
  * free_grid - frees the memory allocate for the grid
- * created by alloc_grid()
+ * creatyed by alloc_grid()
  * @grid: grid to free
  * @height: height of the grid
  */
@@ -13,7 +13,7 @@ void free_grid(int **grid, int height)
 	if (grid == 0 || height == 0)
 		return;
 
-	for (x = 0; x < height; x ++)
+	for (x = 0; x < height; x++)
 		free(grid[x]);
 
 	free(grid);
