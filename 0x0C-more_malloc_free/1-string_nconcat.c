7113f41dec58d995c814d0cb2f6886804cb2440f
wimade <wtlab2020@gmail.com>
2022-07-31 04:14:56 -0700

    wireless

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,59 +1,45 @@
-#include "main.h"
 #include <stdlib.h>
+#include <stdio.h>
 
 /**
- * _strlen - returns the length of a string
- * @s: string to evaluate
- *
- * Return: the length of the string
+ * string_nconcat - concat two strings to n bytes
+ * @s1: first string
+ * @s2: second string
+ * @n: number of bytes to concat of string 2
+ * Return: pointer to concatenated string or NULL if it fails
  */
-int _strlen(char *s)
-{
-	int i = 0;
 
-	while (s[i] != 0)
-	{
-		i++;
-	}
-	return (i);
-}
-
-/**
- * *string_nconcat - concatenates n bytes of a string to another string
- * @s1: string to append to
- * @s2: string to concatenate from
- * @n: number of bytes from s2 to concatenate to s1
- *
- * Return: pointer to the resulting string
- */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int i, j, len1, len2;
-	char *p;
+	char *ptr;
+	unsigned int i, len1, len2;
 
-	if (s1 == NULL)
+	if (!s1)
 		s1 = "";
-	if (s2 == NULL)
+	if (!s2)
 		s2 = "";
 
-	len1 = _strlen(s1);
-	len2 = _strlen(s2);
+	for (len1 = 0; *(s1 + len1); len1++)
+		;
+	for (len2 = 0; *(s2 + len2); len2++)
+		;
 
-	if (n >= len2)
+	if (len2 < n)
 		n = len2;
 
-	p = malloc(sizeof(char) * (len1 + n + 1));
+	ptr = malloc((len1 + n + 1) * sizeof(char));
 
-	if (p == NULL)
+	if (!ptr)
 		return (NULL);
 
-	for (i = 0; i < len1; i++)
-		*(p + i) = *(s1 + j);
+	for (i = 0; i < (len1 + n); i++)
+	{
+		if (i < len1)
+			ptr[i] = s1[i];
+		else
+			ptr[i] = s2[i - len1];
+	}
+	ptr[i] = '\0';
 
-	for (j = 0; j < n; j++)
-		*(p + i + j) = *(s2 + j);
-
-	*(p + i + j) = 0;
-
-	return (p);
+	return (ptr);
 }
