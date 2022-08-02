b47c81df7d109ac99b6b3a84ed798c4bf6fed7ae
wimade <wtlab2020@gmail.com>
2022-08-02 03:43:20 -0700

    wireless

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,12 +2,12 @@
 #define DOG_H
 
 /**
- * struct dog - struct with elements name, age, owner
- * @name: name of dog
- * @age: age of dog
- * @owner: owner of dog
+ * struct dog - a dog's basic info
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
  *
- * Description: struct for dog
+ * Description: Longer description
  */
 struct dog
 {
@@ -16,11 +16,12 @@ struct dog
 	char *owner;
 };
 
-typedef struct dog dog_t;
-
-dog_t *new_dog(char *name, float age, char *owner);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-void free_dog(dog_t *d)
+typedef struct dog dog_t;
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+int _strlen(char *s);
+char *_strcpy(char *dest, char *src);
 
-#endif /*DOG_H*/
+#endif
