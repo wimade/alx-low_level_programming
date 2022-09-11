2d5be2f0304d01f84f0f31cb092461cabfee0304
wimade <wtlab2020@gmail.com>
2022-09-11 16:57:12 -0700

    wireless

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -19,7 +19,7 @@ unsigned int binary_to_uint(const char *b)
 
 	for (len = 0; b[len] != '\0'; len++)
 		;
-	
+
 	for (len--, base_t = 1; len >= 0; len--, base_t *= 2)
 	{
 		if (b[len] != '0' && b[len] != '1')
