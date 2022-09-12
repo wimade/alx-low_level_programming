d8efe427b70aeb1e8c48de7b34d2c7b9eb4ec02a
wimade <wtlab2020@gmail.com>
2022-09-11 19:20:52 -0700

    wireless

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -10,7 +10,7 @@
  */
 void error_file(int file_from, int file_to, char *argv[])
 {
-	if (file_form == -1)
+	if (file_from == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
